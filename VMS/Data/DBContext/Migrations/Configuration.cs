41be1de00b2c7ad2d0217d3fb765230520b92a87
Freddie Yu <fyu_21@outlook.com>
2018-02-06 10:19:29 +0800

    Migration init

---
+++ VMS/Data/DBContext/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace VMS.Data.DBContext.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<VMS.Data.DBContext.VoucherDBCon>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(VMS.Data.DBContext.VoucherDBCon context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
