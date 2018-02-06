28cb3c26a63f3ff015952ccc3650063b475bd5a7
Freddie Yu <fyu_21@outlook.com>
2018-02-06 10:04:30 +0800

    Include Data Layer

---
+++ VMS/Data/DBContext/VoucherDBCon.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+using VMS.Models.Info;
+
+namespace VMS.Data.DBContext
+{
+    
+    public class VoucherDBCon : DbContext
+    {
+        public VoucherDBCon() : base("name=MyAPPDb")
+        {
+
+        }
+        public DbSet<VoucherInfo> VoucherInfoes { get; set; }
+
+        public DbSet<VoucherFormInfo> VoucherForms { get; set; }
+
+        public DbSet<VoucherStatusInfo> VoucherStatus { get; set; }
+
+        public DbSet<ExternalSystemInfo> ExternalSystems { get; set; }
+        public DbSet<VoucherAllotmentInfo> VoucherAllotments { get; set; }
+        public DbSet<VoucherBatchUploadInfo> VoucherBatchUploads { get; set; }
+
+        public DbSet<PeriodInfo> Periods { get; set; }
+
+    }
+}
